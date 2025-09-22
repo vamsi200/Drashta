ca9db33c4143f590ba24a266457ec3911f80784c
vamsi200 <vamsivanama891@gmail.com>
2025-09-23 01:11:38 +0530

    more additions

--- drashta/backend/src/parser.rs
+++ drashta/backend/src/parser.rs
@@ -12,6 +12,9 @@ use once_cell::sync::Lazy;
 use regex::Regex;
 use serde::{Deserialize, Serialize};
 use std::borrow::Cow;
+use std::fs::File;
+use std::io::{BufRead, BufReader, Read};
+use std::path::PathBuf;
 use std::result::Result::Ok;
 use std::sync::Arc;
 use std::{
@@ -40,6 +43,8 @@ pub enum Service {
     Sshd,
     Sudo,
     Login,
+    UserChange,
+    PkgManager,
 }
 
 #[derive(Clone, Debug, PartialEq, Eq, Hash)]
@@ -56,6 +61,18 @@ pub enum EventType {
     IncorrectPassword,
     AuthError,
     AuthFailure,
+    NewUser,
+    NewGroup,
+    DeleteGroup,
+    DeleteUser,
+    ModifyUser,
+    ModifyGroup,
+    PasswdChange,
+    PkgInstalled,
+    PkgRemoved,
+    PkgUpgraded,
+    PkgReinstalled,
+    PkgDowndraded,
 }
 
 pub fn rg_capture(msg: &regex::Captures, i: usize) -> Option<String> {
@@ -416,12 +433,236 @@ pub fn parse_login_attempts(entry_map: Entry) -> Option<EventData> {
 pub fn parse_kernel_events(map: Entry) -> Option<EventData> {
     todo!()
 }
-pub fn parse_user_change_events(map: Entry) -> Option<EventData> {
-    todo!()
-}
+pub fn parse_user_change_events(entry_map: Entry) -> Option<EventData> {
+    static USER_CREATION_REGEX: Lazy<Vec<(&str, Regex)>> = Lazy::new(|| {
+        vec![
+            ("NEW_USER", Regex::new(r"^new user: name=(\S+), UID=(\d+), GID=(\d+), home=(\S+), shell=(\S+), from=(\S+)$").unwrap()),
+            ("NEW_GROUP", Regex::new(r"^new group: name=(\S+), GID=(\d+)$").unwrap()),
+            ("GROUP_ADDED_ETC_GROUP", Regex::new(r"^group added to /etc/group: name=(\S+), GID=(\d+)$").unwrap()),
+            ("GROUP_ADDED_ETC_GSHADOW", Regex::new(r"^group added to /etc/gshadow: name=(\S+)$").unwrap()),
+        ]
+    });
 
-pub fn parse_pkg_events(map: Entry) -> Option<EventData> {
-    todo!()
+    static USER_DELETION_REGEX: Lazy<Vec<(&str, Regex)>> = Lazy::new(|| {
+        vec![
+            (
+                "DELETE_USER",
+                Regex::new(
+                    r"^delete user: name=(\S+), UID=(\d+), GID=(\d+), home=(\S+), shell=(\S+)$",
+                )
+                .unwrap(),
+            ),
+            (
+                "DELETE_USER_HOME",
+                Regex::new(r"^delete home directory: (.+)$").unwrap(),
+            ),
+            (
+                "DELETE_USER_MAIL",
+                Regex::new(r"^delete mail spool: (.+)$").unwrap(),
+            ),
+            (
+                "DELETE_GROUP",
+                Regex::new(r"^delete group: name=(\S+), GID=(\d+)$").unwrap(),
+            ),
+        ]
+    });
+
+    static USER_MODIFICATION_REGEX: Lazy<Vec<(&str, Regex)>> = Lazy::new(|| {
+        vec![
+            (
+                "MODIFY_USER",
+                Regex::new(r"^usermod: name=(\S+),.*$").unwrap(),
+            ),
+            (
+                "MODIFY_GROUP",
+                Regex::new(r"^groupmod: name=(\S+),.*$").unwrap(),
+            ),
+            (
+                "USER_PASSWD_CHANGE",
+                Regex::new(r"^passwd\[(\d+)\]: password changed for (\S+)$").unwrap(),
+            ),
+            (
+                "USER_SHADOW_UPDATED",
+                Regex::new(r"^shadow file updated for user (\S+)$").unwrap(),
+            ),
+        ]
+    });
+
+    let mut map = AHashMap::new();
+    let mut timestamp = String::new();
+    if let Some(tp) = entry_map.get("SYSLOG_TIMESTAMP") {
+        timestamp = tp.to_owned();
+    }
+
+    if let Some(msg) = entry_map.get("MESSAGE") {
+        for (name, regex) in USER_CREATION_REGEX.iter() {
+            if let Some(s) = regex.captures(msg) {
+                let (data, event_type): (Option<&[(&str, usize)]>, EventType) = match *name {
+                    "NEW_USER" => (
+                        Some(&[
+                            ("name", 1),
+                            ("uid", 2),
+                            ("gid", 3),
+                            ("home", 4),
+                            ("shell", 5),
+                            ("pts", 6),
+                        ]),
+                        EventType::NewUser,
+                    ),
+                    "NEW_GROUP" => (Some(&[("name", 1), ("gid", 2)]), EventType::NewGroup),
+                    "GROUP_ADDED_ETC_GROUP" => (Some(&[("name", 1), ("gid", 2)]), EventType::Info),
+                    "GROUP_ADDED_ETC_GSHADOW" => (Some(&[("name", 1)]), EventType::Info),
+                    _ => (None, EventType::Other),
+                };
+                if let Some(fields) = data {
+                    for &(name, idx) in fields {
+                        if let Some(m) = s.get(idx) {
+                            map.insert(name.to_string(), m.as_str().to_string());
+                        }
+                    }
+                }
+                return Some(EventData {
+                    timestamp,
+                    service: Service::UserChange,
+                    event_type,
+                    data: map,
+                    raw_msg: msg.clone(),
+                });
+            }
+            for (name, regex) in USER_DELETION_REGEX.iter() {
+                if let Some(s) = regex.captures(msg) {
+                    let (data, event_type): (Option<&[(&str, usize)]>, EventType) = match *name {
+                        "DELETE_USER" => (
+                            Some(&[
+                                ("name", 1),
+                                ("uid", 2),
+                                ("gid", 3),
+                                ("home", 4),
+                                ("shell", 5),
+                            ]),
+                            EventType::DeleteUser,
+                        ),
+                        "DELETE_USER_HOME" => (Some(&[("name", 1)]), EventType::DeleteGroup),
+                        "DELETE_USER_MAIL" => (Some(&[("name", 1)]), EventType::Info),
+                        "DELETE_GROUP" => {
+                            (Some(&[("name", 1), ("gid", 2)]), EventType::DeleteGroup)
+                        }
+                        _ => (None, EventType::Other),
+                    };
+                    if let Some(fields) = data {
+                        for &(name, idx) in fields {
+                            if let Some(m) = s.get(idx) {
+                                map.insert(name.to_string(), m.as_str().to_string());
+                            }
+                        }
+                    }
+                    return Some(EventData {
+                        timestamp,
+                        service: Service::UserChange,
+                        event_type,
+                        data: map,
+                        raw_msg: msg.clone(),
+                    });
+                }
+            }
+
+            for (name, regex) in USER_MODIFICATION_REGEX.iter() {
+                if let Some(s) = regex.captures(msg) {
+                    let (data, event_type): (Option<&[(&str, usize)]>, EventType) = match *name {
+                        "MODIFY_USER" => (Some(&[("name", 1)]), EventType::ModifyUser),
+                        "MODIFY_GROUP" => (Some(&[("name", 1)]), EventType::DeleteGroup),
+                        "USER_PASSWD_CHANGE" => {
+                            (Some(&[("processid", 1), ("user", 2)]), EventType::Info)
+                        }
+                        "USER_SHADOW_UPDATED" => (Some(&[("name", 1)]), EventType::DeleteGroup),
+                        _ => (None, EventType::Other),
+                    };
+                    if let Some(fields) = data {
+                        for &(name, idx) in fields {
+                            if let Some(m) = s.get(idx) {
+                                map.insert(name.to_string(), m.as_str().to_string());
+                            }
+                        }
+                    }
+                    return Some(EventData {
+                        timestamp,
+                        service: Service::UserChange,
+                        event_type,
+                        data: map,
+                        raw_msg: msg.clone(),
+                    });
+                }
+            }
+        }
+    }
+    None
+}
+pub fn parse_pkg_events(content: String) -> Option<EventData> {
+    static PKG_EVENTS_REGEX: Lazy<Vec<(&str, Regex)>> = Lazy::new(|| {
+        vec![
+            (
+                "INSTALLED",
+                Regex::new(r"^\[(.+?)\] \[ALPM\] installed (\S+) \(([^)]+)\)$").unwrap(),
+            ),
+            (
+                "REMOVED",
+                Regex::new(r"^\[(.+?)\] \[ALPM\] removed (\S+) \(([^)]+)\)$").unwrap(),
+            ),
+            (
+                "UPGRADED",
+                Regex::new(r"^\[(.+?)\] \[ALPM\] upgraded (\S+) \(([^)]+) -> ([^)]+)\)$").unwrap(),
+            ),
+            (
+                "DOWNGRADED",
+                Regex::new(r"^\[(.+?)\] \[ALPM\] downgraded (\S+) \(([^)]+) -> ([^)]+)\)$")
+                    .unwrap(),
+            ),
+            (
+                "REINSTALLED",
+                Regex::new(r"^\[(.+?)\] \[ALPM\] reinstalled (\S+) \(([^)]+)\)$").unwrap(),
+            ),
+        ]
+    });
+    let mut map = AHashMap::new();
+
+    for (name, regex) in PKG_EVENTS_REGEX.iter() {
+        if let Some(s) = regex.captures(&content) {
+            let timestamp = s.get(1).unwrap().as_str().to_string();
+            let (data, event_type): (Option<&[(&str, usize)]>, EventType) = match *name {
+                "INSTALLED" => (Some(&[("pkg_name", 2)]), EventType::PkgInstalled),
+                "REMOVED" => (Some(&[("pkg_name", 2)]), EventType::PkgInstalled),
+                "UPGRADED" => (
+                    Some(&[("pkg_name", 2), ("version_from", 3), ("version_to", 4)]),
+                    EventType::PkgUpgraded,
+                ),
+                "DOWNGRADED" => (
+                    Some(&[("pkg_name", 2), ("version_from", 3), ("version_to", 4)]),
+                    EventType::PkgDowndraded,
+                ),
+                "REINSTALLED" => (
+                    Some(&[("pkg_name", 2), ("version", 3)]),
+                    EventType::PkgReinstalled,
+                ),
+                _ => (None, EventType::Other),
+            };
+
+            if let Some(fields) = data {
+                for &(name, idx) in fields {
+                    if let Some(m) = s.get(idx) {
+                        map.insert(name.to_string(), m.as_str().to_string());
+                    }
+                }
+            }
+            return Some(EventData {
+                timestamp,
+                service: Service::PkgManager,
+                event_type,
+                data: map,
+                raw_msg: content,
+            });
+        }
+    }
+    None
 }
 
 pub fn parse_config_change_events(map: Entry) -> Option<EventData> {
@@ -430,9 +671,11 @@ pub fn parse_config_change_events(map: Entry) -> Option<EventData> {
 pub fn parse_network_events(map: Entry) -> Option<EventData> {
     todo!()
 }
+pub fn parse_firewalld_events(map: Entry) -> Option<EventData> {
+    todo!()
+}
 
-// fn parse_kernel_events()
-
+// Should also think to capture command failures
 //TODO: Need to check the name's of the services beacuse there are different on different distros
 pub fn flush_previous_data(
     tx: tokio::sync::broadcast::Sender<EventData>,
@@ -481,11 +724,18 @@ pub fn flush_previous_data(
                 }
 
                 "pkg.events" => {
-                    // Not working!!
-                    s.match_add("_EXE", "/usr/bin/yay")?;
-                    s.match_add("_EXE", "/usr/bin/pacman")?;
-                    while let Some(data) = s.next_entry()? {
-                        println!("{:?}", data);
+                    //TODO: Have to use inotify or use sleep to read live events
+                    let file_name = PathBuf::from("/var/log/pacman.log");
+                    let file = File::open(file_name).unwrap();
+                    let reader = BufReader::new(file);
+
+                    for line in reader.lines() {
+                        let line = line.unwrap_or(String::new());
+                        if let Some(ev) = parse_pkg_events(line) {
+                            if let Err(e) = tx.send(ev) {
+                                println!("Dropped");
+                            }
+                        }
                     }
                 }
 
@@ -506,7 +756,7 @@ pub fn flush_previous_data(
                 "kernel.events" => {
                     s.match_add("_TRANSPORT", "kernel")?;
                     while let Some(data) = s.next_entry()? {
-                        println!("{:?}", data);
+                        parse_kernel_events(data);
                     }
                 }
 
@@ -515,7 +765,11 @@ pub fn flush_previous_data(
                     s.match_add("_COMM", "groupadd")?;
                     s.match_add("_COMM", "passwd")?;
                     while let Some(data) = s.next_entry()? {
-                        println!("{:?}", data);
+                        if let Some(ev) = parse_user_change_events(data) {
+                            if let Err(e) = tx.send(ev) {
+                                println!("Dropped");
+                            }
+                        }
                     }
                 }
 
